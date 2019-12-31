13365b24d12ad87a43d35b8181c9bd477362cf80
吴晗 <han.wu@tongdun.cn>
2019-12-31 16:40:01 +0800

    修改解密哈数位反射调用android API,防止java api 在低版本android os 不存在而产生问题

--- string-box/src/main/java/com/github/box/impl/Base64StringCipher.java
+++ string-box/src/main/java/com/github/box/impl/Base64StringCipher.java
@@ -1,21 +1,42 @@
 package com.github.box.impl;
 
 import com.github.box.StringCipher;
-
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Base64;
 
-
+/**
+ * @author lotty
+ */
 public class Base64StringCipher implements StringCipher {
 
-    @Override
-    public String dd(String value) {
-        byte[] decode = Base64.getDecoder().decode(value);
-        return new String(decode);
-    }
+  private Method method;
 
-    @Override
-    public String ee(String value) {
-        byte[] encode = Base64.getEncoder().encode(value.getBytes());
-        return new String(encode);
+  @Override
+  public String dd(String value) {
+    if (method == null) {
+      try {
+        Class cls = Class.forName("android.util.Base64");
+        method = cls.getDeclaredMethod("decode", String.class, Integer.class);
+      } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+      } catch (NoSuchMethodException e) {
+        e.printStackTrace();
+      }
     }
+    try {
+      return new String((byte[]) method.invoke(null, value, 2));
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    } catch (InvocationTargetException e) {
+      e.printStackTrace();
+    }
+    return "";
+  }
+
+  @Override
+  public String ee(String value) {
+    byte[] encode = Base64.getEncoder().encode(value.getBytes());
+    return new String(encode);
+  }
 }
