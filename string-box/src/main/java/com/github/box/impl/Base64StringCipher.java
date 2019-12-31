4f873f5c7cec9f9f7c70105f9e74e8441c2378fa
吴晗 <han.wu@tongdun.cn>
2019-12-31 16:44:17 +0800

    直接修改Base64加解密方式为android.util.Base64

--- string-box/src/main/java/com/github/box/impl/Base64StringCipher.java
+++ string-box/src/main/java/com/github/box/impl/Base64StringCipher.java
@@ -1,42 +1,19 @@
 package com.github.box.impl;
 
 import com.github.box.StringCipher;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Base64;
 
 /**
  * @author lotty
  */
 public class Base64StringCipher implements StringCipher {
 
-  private Method method;
-
   @Override
   public String dd(String value) {
-    if (method == null) {
-      try {
-        Class cls = Class.forName("android.util.Base64");
-        method = cls.getDeclaredMethod("decode", String.class, Integer.class);
-      } catch (ClassNotFoundException e) {
-        e.printStackTrace();
-      } catch (NoSuchMethodException e) {
-        e.printStackTrace();
-      }
-    }
-    try {
-      return new String((byte[]) method.invoke(null, value, 2));
-    } catch (IllegalAccessException e) {
-      e.printStackTrace();
-    } catch (InvocationTargetException e) {
-      e.printStackTrace();
-    }
-    return "";
+    return new String(android.util.Base64.decode(value, android.util.Base64.NO_WRAP));
   }
 
   @Override
   public String ee(String value) {
-    byte[] encode = Base64.getEncoder().encode(value.getBytes());
-    return new String(encode);
+    return new String(android.util.Base64.encode(value.getBytes(), android.util.Base64.NO_WRAP));
   }
 }
